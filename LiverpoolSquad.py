904e714affa9e2b637064ba67f59fdb048727786
Vlastimir <149712502+Ithility@users.noreply.github.com>
2024-02-16 18:47:00 +0100

    Basic code.
    This is the first code, it is simple but for a beginner working with scrapy was a pure hell.
    I use command JSON -o to create a new file that lists all the players.

---
+++ LiverpoolSquad.py
@@ -0,0 +1,15 @@
+import scrapy
+
+class LiverpoolSpider(scrapy.Spider):
+    name = 'scouser'
+    start_urls = ['https://www.premierleague.com/clubs/10/Liverpool/squad']
+
+    def parse(self, response, **kwargs):
+        for player_card in response.css('li.stats-card'):
+            yield {
+                'name': player_card.css('div.stats-card__player-first::text').get(),
+                'surname': player_card.css('div.stats-card__player-last::text').get(),
+                'position': player_card.css('div.stats-card__player-position::text').get(),
+                'country': player_card.css('span.stats-card__player-country::text').get()
+            }
+
